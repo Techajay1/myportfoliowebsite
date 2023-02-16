2d54c42e3803f00a0b4917674b5243958a05be21
DESKTOP-GD7OT03\Ajay <ajay@dalvkot.com>
2023-02-17 03:16:29 +0530

    deployment commit

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,3 +1,4 @@
+
 import email from '../assets/email.png';
 import mobile from '../assets/mobile.png';
 import api from '../assets/api.png';
@@ -34,7 +35,7 @@ import bolt from '../assets/bolt.png';
 import nb from '../assets/nb.png';
 import skype from '../assets/skype.png';
 import spotify from '../assets/spotify.png';
-
+/* eslint import/no-anonymous-default-export: [2, {"allowObject": true}] */
 export default {
     email,
     mobile,
@@ -70,3 +71,4 @@ export default {
     skype,
     spotify,
 };
+
--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,11 +1,52 @@
-import React from 'react'
+// import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+import { AppWrap, MotionWrap } from '../../Wrapper';
+import './About.scss';
+// import { urlFor, client } from '../../client';
+import { images } from '../../constants';
+const abouts= [
+  {title:'Web Development',description:'I am good web developer',imageUrl:images.about01},
+  {title:'Angular Developer',description:'I am good web developer',imageUrl:images.about02},
+  {title:'Mean Developer',description:'I am good web developer',imageUrl:images.about03},
+  {title:'Mean Developer',description:'I am good web developer',imageUrl:images.about04},
+];
 
-import './About.scss'
+const About = () => {
+  // const [abouts, setAbouts] = useState([]);
 
-const about = () => {
+  // useEffect(() => {
+  //   const query = '*[_type == "abouts"]';
+
+  //   client.fetch(query).then((data) => {
+  //     setAbouts(data);
+  //   });
+  // }, []);
+
   return (
-    <div>about</div>
-  )
-}
+    <>
+      <h2 className="head-text">I Know that <span>Good Design</span> <br />means  <span>Good Business</span></h2>
 
-export default about
+      <div className="app__profiles">
+        {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: 'tween' }}
+            className="app__profile-item"
+            key={about.title + index}
+          >
+            <img src={about.imageUrl} alt={about.title} />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
+            <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
+          </motion.div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default AppWrap(
+  MotionWrap(About, 'app__about'),
+  'about',
+  'app__whitebg',
+);
